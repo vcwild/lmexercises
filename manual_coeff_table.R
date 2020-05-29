615da2d2bd0046a08163859384a0303e15952faa
vcwild <vcwildm@gmail.com>
2020-05-29 11:28:28 -0300

    add manual coefficient table

---
+++ manual_coeff_table.R
@@ -0,0 +1,42 @@
+data("diamond")
+
+
+y <- diamond$price
+x <- diamond$carat
+
+
+# Manual regression coefficient table -------------------------------------------------------
+
+n <- length(y)
+beta1 <- cor(y, x) * sd(y) / sd(x)
+beta0 <- mean(y) - beta1 * mean(x)
+e <- y - beta0 - beta1 * x
+sigma <- sqrt(sum(e^2) / (n - 2))
+ssx <- sum((x - mean(x))^2)
+
+seBeta0 <- (1 / n + mean(x)^2 / ssx)^0.5 * sigma
+seBeta1 <- sigma / sqrt(ssx)
+tBeta0 <- beta0 / seBeta0
+tBeta1 <- beta1 / seBeta1
+pBeta0 <- 2 * pt(abs(tBeta0), df = n - 2, lower.tail = FALSE)
+pBeta1 <- 2 * pt(abs(tBeta1), df = n - 2, lower.tail = FALSE)
+
+coefTable <- rbind(c(beta0, seBeta0, tBeta0, pBeta0), c(beta1, seBeta1, tBeta1, pBeta1))
+colnames(coefTable) <- c("Estimate", "Std. Error", "t value", "P(>|t|)")
+rownames(coefTable) <- c("(Intercept)", "x")
+coefTable
+
+
+# Using rstat library ---------------------------------------------------------
+
+fit <- lm(y ~ x)
+summary(fit)$coefficients
+
+
+# Confidence intervals -----------------------------------------------------------------------
+
+sumCoef <- summary(fit)$coefficients
+
+sumCoef[1, 1] + c(-1, 1) * qt(.975, df = fit$df) * sumCoef[1, 2]
+# 95% Confidence interval for the slope
+(sumCoef[2, 1] + c(-1, 1) * qt(.975, df = fit$df) * sumCoef[2, 2]) / 10
