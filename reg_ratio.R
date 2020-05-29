2c022f1823b5ffcb92ff9c10360310c62653ee66
vcwild <vcwildm@gmail.com>
2020-05-29 11:30:42 -0300

    add ratio comparison

---
+++ reg_ratio.R
@@ -0,0 +1,15 @@
+# Refer back to the mtcars data set with mpg as an outcome and weight (wt) as the predictor. 
+# About what is the ratio of the the sum of the squared errors, \sum_{i=1}^n (Y_i - \hat Y_i)^2
+# when comparing a model with just an intercept (denominator) to the model with the intercept and slope (numerator)?
+data("mtcars")
+x = mtcars$wt
+y = mtcars$mpg
+
+fit <- lm(y ~ x)
+unit <- lm(y ~ 1)
+
+num <- sum((predict(fit) - y)^2)
+denom <- sum((predict(unit) - y)^2)
+
+num/denom
+#>[1] 0.2471672
