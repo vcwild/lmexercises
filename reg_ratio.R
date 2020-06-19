c69e48159330d7b1cc3e109ad62d2037c6d4e5ac
Victor Wildner <vcwildm@gmail.com>
2020-06-19 11:03:41 -0300

    Update reg_ratio.R

--- reg_ratio.R
+++ reg_ratio.R
@@ -1,6 +1,7 @@
 # Refer back to the mtcars data set with mpg as an outcome and weight (wt) as the predictor. 
 # About what is the ratio of the the sum of the squared errors, \sum_{i=1}^n (Y_i - \hat Y_i)^2
 # when comparing a model with just an intercept (denominator) to the model with the intercept and slope (numerator)?
+
 data("mtcars")
 x = mtcars$wt
 y = mtcars$mpg
